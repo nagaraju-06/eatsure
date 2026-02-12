fa8601e03590bbf509a884df250f50d3582f3a73
Naga Raju <kuppalanagaraju2000@gmail.com>
2026-02-12 18:39:57 +0530

    initial commit

---
+++ src/main/java/com/alpha/eatsure/EatsureApplication.java
@@ -0,0 +1,13 @@
+package com.alpha.eatsure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatsureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatsureApplication.class, args);
+	}
+
+}
