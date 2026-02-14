9a5cd3324e0d5405822e5f8abedbdb255463d256
unknown <ajayaleti1@gmail.com>
2026-02-14 18:35:03 +0530

    Added Address entity, repo and DTO

---
+++ src/main/java/com/alpha/eatsure/Dto/jj.java
@@ -0,0 +1,5 @@
+package com.alpha.eatsure.Dto;
+
+public class jj {
+
+}
---
+++ src/main/java/com/alpha/eatsure/entity/Address.java
@@ -0,0 +1,90 @@
+package com.alpha.eatsure.entity;
+
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Address {
+	@Id
+	 @GeneratedValue(strategy = GenerationType.AUTO)
+     private int id;
+	 private String city;
+	 private int pincode;
+	 private String state;
+	 private String country;
+	 private String street;
+	 private String landmark;
+	 
+	 
+	 
+	 
+	 public Address() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	 public Address(int id, String city, int pincode, String state, String country, String street, String landmark) {
+		super();
+		this.id = id;
+		this.city = city;
+		this.pincode = pincode;
+		this.state = state;
+		this.country = country;
+		this.street = street;
+		this.landmark = landmark;
+	}
+	 @Override
+	public String toString() {
+		return "Address [id=" + id + ", city=" + city + ", pincode=" + pincode + ", state=" + state + ", country="
+				+ country + ", street=" + street + ", landmark=" + landmark + "]";
+	}
+	
+	 public int getId() {
+		 return id;
+	 }
+	 public void setId(int id) {
+		 this.id = id;
+	 }
+	 public String getCity() {
+		 return city;
+	 }
+	 public void setCity(String city) {
+		 this.city = city;
+	 }
+	 public int getPincode() {
+		 return pincode;
+	 }
+	 public void setPincode(int pincode) {
+		 this.pincode = pincode;
+	 }
+	 public String getState() {
+		 return state;
+	 }
+	 public void setState(String state) {
+		 this.state = state;
+	 }
+	 public String getCountry() {
+		 return country;
+	 }
+	 public void setCountry(String country) {
+		 this.country = country;
+	 }
+	 public String getStreet() {
+		 return street;
+	 }
+	 public void setStreet(String street) {
+		 this.street = street;
+	 }
+	 public String getLandmark() {
+		 return landmark;
+	 }
+	 public void setLandmark(String landmark) {
+		 this.landmark = landmark;
+	 }
+	 
+	 
+}
+
