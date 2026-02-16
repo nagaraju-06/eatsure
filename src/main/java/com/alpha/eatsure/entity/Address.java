f8303e7103ecf9e14a3fff0710af872bd1494848
unknown <ajayaleti1@gmail.com>
2026-02-16 19:38:07 +0530

    Updated Address entity

--- src/main/java/com/alpha/eatsure/entity/Address.java
+++ src/main/java/com/alpha/eatsure/entity/Address.java
@@ -11,79 +11,24 @@ import jakarta.persistence.Id;
 public class Address {
 	@Id
 	 @GeneratedValue(strategy = GenerationType.AUTO)
-     private int id;
+     private Integer id;
+	private double latitude;
+	private double longitude;
+	private String flatnumber;
+	private String floor;
+	private String area;
+	private String district;
+	
 	 private String city;
 	 private int pincode;
 	 private String state;
 	 private String country;
 	 private String street;
 	 private String landmark;
+	 private String adresstype;
 	 
 	 
-	 
-	 
-	 public Address() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	 public Address(int id, String city, int pincode, String state, String country, String street, String landmark) {
-		super();
-		this.id = id;
-		this.city = city;
-		this.pincode = pincode;
-		this.state = state;
-		this.country = country;
-		this.street = street;
-		this.landmark = landmark;
-	}
-	 @Override
-	public String toString() {
-		return "Address [id=" + id + ", city=" + city + ", pincode=" + pincode + ", state=" + state + ", country="
-				+ country + ", street=" + street + ", landmark=" + landmark + "]";
-	}
 	
-	 public int getId() {
-		 return id;
-	 }
-	 public void setId(int id) {
-		 this.id = id;
-	 }
-	 public String getCity() {
-		 return city;
-	 }
-	 public void setCity(String city) {
-		 this.city = city;
-	 }
-	 public int getPincode() {
-		 return pincode;
-	 }
-	 public void setPincode(int pincode) {
-		 this.pincode = pincode;
-	 }
-	 public String getState() {
-		 return state;
-	 }
-	 public void setState(String state) {
-		 this.state = state;
-	 }
-	 public String getCountry() {
-		 return country;
-	 }
-	 public void setCountry(String country) {
-		 this.country = country;
-	 }
-	 public String getStreet() {
-		 return street;
-	 }
-	 public void setStreet(String street) {
-		 this.street = street;
-	 }
-	 public String getLandmark() {
-		 return landmark;
-	 }
-	 public void setLandmark(String landmark) {
-		 this.landmark = landmark;
-	 }
 	 
 	 
 }
